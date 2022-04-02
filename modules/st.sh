75caf890734543a69dfd29ac2306be79bcea8df9
mishanya644 <ivanov0604041@gmail.com>
2022-04-02 21:19:24 +0300

    fixed st font error (finally)

--- modules/st.sh
+++ modules/st.sh
@@ -1,3 +1,3 @@
 sudo cp ./configs/iosevka-splendid-regular.ttf /usr/share/fonts
 sudo cp ./packages/st /usr/bin/
-echo "alias st='st -f iosevka\-splendid:size=11.5'" >> ~/.bashrc
+echo "alias st='st -f \"iosevka\-splendid:size=11.5\"'" >> ~/.bashrc
