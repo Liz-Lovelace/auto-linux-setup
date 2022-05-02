44f93470bcf0dbb54c856f44a325f645d7b84482
mishanya644 <ivanov0604041@gmail.com>
2022-05-02 16:17:57 +0300

    i3 now centralized

--- modules/i3.sh
+++ modules/i3.sh
@@ -5,6 +5,3 @@ sudo apt install -y i3
 cd ~/.config
 git clone https://github.com/Mishanya644/config-i3
 mv config-i3 i3
-cd i3
-git clone https://github.com/Mishanya644/sweet-i3bar
-git clone https://github.com/Mishanya644/dmount
